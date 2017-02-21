04299fef4e18882320b555acd0f89b33027ddcbf
HoangViet <hoangtheviet93@gmail.com>
2017-02-21 08:52:36 +0700

    Update stm32_flash.sh

--- tool/stm32_flash.sh
+++ tool/stm32_flash.sh
@@ -4,7 +4,7 @@
 OPENOCD_CFG=$1
 BIN_IMAGE=$2
 
-# send SIGKILL signal to kill openocd process
+# send SIGKILL signal to kill old openocd process
 killall -s 9 openocd
 # openocd flash command line
 openocd -f ${OPENOCD_CFG} -c "reset halt" -c "flash write_image erase ${BIN}" \
